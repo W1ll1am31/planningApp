0147f2c6b5b65639ce32c75f86d4f034b5057236
William Cresswell <william.cresswell5@gmail.com>
2018-10-17 23:23:11 +0100

    First pass at displaying images in a mobile friendly way with popups

--- src/components/Pictures.vue
+++ src/components/Pictures.vue
@@ -1,7 +1,62 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <h1>Pictures</h1>
-    </v-slide-y-transition>
+    <h1>Pictures</h1>
+    <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="(image, key) in images"
+              :key="key"
+              xs4
+              @click="viewImage(image)"
+            >
+              <v-card flat tile>
+                <v-img
+                  :src="imageDir + image"
+                  height="150px"
+                ></v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  <v-dialog v-model="imagePopup">
+    <v-img v-if="selectedImage" :src="imageDir + selectedImage"></v-img>
+  </v-dialog>
   </v-container>
 </template>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        imageDir: "/static/img/",
+        imagePopup: false,
+        selectedImage: null,
+        images: [],
+        foo: {}
+      }
+    },
+    mounted: function() {
+      this.getImageList();
+    },
+    methods: {
+      viewImage: function(image) {
+        this.imagePopup = true;
+        this.selectedImage = image;
+        console.log("viewing " + image);
+      },
+      getImageList: function() {
+        const con = require.context("../../static/img/", false, /\.png$/);
+
+        con.keys().forEach(element => {
+          this.images.push(element.split("/")[1])
+        });
+      }
+    }
+  }
+</script>
