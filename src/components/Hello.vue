142ad51da97b328bb2e343917451fbc3b19e66cc
William Cresswell <william.cresswell5@gmail.com>
2018-10-17 22:31:08 +0100

    Added content to basic introduction page

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,8 +2,15 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="../../static/img/v.png" alt="Vuetify.js" class="mb-5">
-        <h1>Hello world!</h1>
+        <h1>Hello!</h1>
+        <v-card>
+          <v-card-title><h2>Personal Finance Application planner</h2></v-card-title>
+          <v-card-text>
+            My idea for the final application is a personal finance tracker
+            The application will be aimed at anyone who would like to manage and keep track of their finances
+            Use the menu to navigate to the other pages
+          </v-card-text>
+        </v-card>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
