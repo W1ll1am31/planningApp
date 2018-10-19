d9af09b32e87770073dd2937ed13fa8e21db6650
William Cresswell <william.cresswell5@gmail.com>
2018-10-19 23:30:25 +0100

    Updated image navigation to be able to scroll

--- src/components/Pictures.vue
+++ src/components/Pictures.vue
@@ -25,38 +25,83 @@
     </v-flex>
   </v-layout>
   <v-dialog v-model="imagePopup">
-    <v-img v-if="selectedImage" :src="imageDir + selectedImage"></v-img>
+    <v-img v-if="selectedImage" :src="imageDir + images[currentImage]">
+      <v-btn
+        dark
+        small
+        top
+        left
+        fab
+        @click="imagePopup = false"
+      >
+        <v-icon>close</v-icon>
+      </v-btn>
+      <v-btn
+        dark
+        top
+        left
+        small
+        fab
+        @click="previousImage()"
+      >
+        <v-icon>arrow_left</v-icon>
+      </v-btn>
+      <v-btn
+        dark
+        top
+        small
+        fab
+        @click="nextImage()"
+      >
+        <v-icon>arrow_right</v-icon>
+      </v-btn>
+    </v-img>
   </v-dialog>
   </v-container>
 </template>
 
 <script>
-  module.exports = {
-    data: function() {
-      return {
-        imageDir: "/static/img/",
-        imagePopup: false,
-        selectedImage: null,
-        images: [],
-        foo: {}
+module.exports = {
+  data: function() {
+    return {
+      imageDir: "/static/img/",
+      imagePopup: false,
+      selectedImage: null,
+      images: [],
+      currentImage: 0
+    };
+  },
+  mounted: function() {
+    this.getImageList();
+  },
+  methods: {
+    viewImage: function(image) {
+      this.imagePopup = true;
+      this.selectedImage = image;
+      this.currentImage = this.images.indexOf(image);
+      console.log("viewing " + image);
+    },
+    getImageList: function() {
+      const con = require.context("../../static/img/", false, /\.png$/);
+
+      con.keys().forEach(element => {
+        this.images.push(element.split("/")[1]);
+      });
+    },
+    nextImage: function() {
+      if(this.currentImage == this.images.length-1) {
+        this.currentImage = 0;
+      } else {
+        this.currentImage++
       }
     },
-    mounted: function() {
-      this.getImageList();
-    },
-    methods: {
-      viewImage: function(image) {
-        this.imagePopup = true;
-        this.selectedImage = image;
-        console.log("viewing " + image);
-      },
-      getImageList: function() {
-        const con = require.context("../../static/img/", false, /\.png$/);
-
-        con.keys().forEach(element => {
-          this.images.push(element.split("/")[1])
-        });
+    previousImage: function() {
+      if(this.currentImage == 0) {
+        this.currentImage = this.images.length -1;
+      } else {
+        this.currentImage--;
       }
     }
   }
+};
 </script>
