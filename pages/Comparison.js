7ced87a94b4ba23879395da77d6f8f4d0f0da869
William Cresswell <william.cresswell5@gmail.com>
2018-10-26 12:34:58 +0100

    Added comparison to other app

--- pages/Comparison.js
+++ pages/Comparison.js
@@ -2,9 +2,77 @@ var comparison = Vue.component('comparison', {
   template: 
   `
 <v-container fluid>
-  <v-layout column align-center>
-    <h1>comparison!</h1>
-  </v-layout>
+<v-layout column align-center>
+<h1>Comparison with existing apps</h1>
+<v-card>
+  <v-card-title><h3>Mint</h3></v-card-title>
+  <v-data-table
+    :headers="headers"
+    :items="features"
+    hide-actions
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.name }}</td>
+      <td>{{ props.item.app }}</td>
+    </template>
+  </v-data-table>
+</v-card>
+</v-layout>
 </v-container>
-  `
+  `,
+  data: function() {
+    return {
+      headers: [
+        {
+          text: 'Application Feature',
+          align: 'left',
+          value: 'name'
+        },
+        { text: 'What app can do it?', value: 'app' }
+      ],
+      features: [
+        {
+          value: false,
+          name: 'Notification of upcoming bills',
+          app: "Mint",
+        },
+        {
+          value: false,
+          name: 'Tracking incoming money',
+          app: "Both",
+        },
+        {
+          value: false,
+          name: 'Store receipt pictures/details',
+          app: "Mine",
+        },
+        {
+          value: false,
+          name: 'Budgeting functionality',
+          app: "Both",
+        },
+        {
+          value: false,
+          name: 'Links to bank account',
+          app: "Mint",
+        },
+        {
+          value: false,
+          name: 'Calculate credit score',
+          app: "Mint",
+        },
+        {
+          value: false,
+          name: 'Tips to reduce spending',
+          app: "Mine",
+        },
+        {
+          value: false,
+          name: 'Weekly or monthly budget',
+          app: "Mine",
+        }
+      ]
+    }
+  }
 })
